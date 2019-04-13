431f568abca5eb85063ca4142fe3666a56ecd70a
GruXsqK <HellLord@bk.ru>
2019-04-13 14:34:08 +0300

    Домашняя работа к Lesson_3 (e/n)

---
+++ hw03_easy.py
@@ -0,0 +1,40 @@
+
+__author__ = 'Белинский Андрей Петрович'
+
+# Задание-1:
+# Напишите функцию, округляющую полученное произвольное десятичное число
+# до кол-ва знаков (кол-во знаков передается вторым аргументом).
+# Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
+# Для решения задачи не используйте встроенные функции и функции из модуля math.
+
+def my_round(number, ndigits):
+    x = round(number, ndigits)
+    return x
+
+print(my_round(2.1234567, 5))
+print(my_round(2.1999967, 5))
+print(my_round(2.9999967, 5))
+
+
+# Задание-2:
+# Дан шестизначный номер билета. Определить, является ли билет счастливым.
+# Решение реализовать в виде функции.
+# Билет считается счастливым, если сумма его первых и последних цифр равны.
+# !!!P.S.: функция не должна НИЧЕГО print'ить
+
+def lucky_ticket(ticket_number):
+    ticket = list(str(ticket_number))
+    if len(ticket) == 6:
+        sum1 = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
+        sum2 = int(ticket[3]) + int(ticket[4]) + int(ticket[5])
+        if sum1 == sum2:
+            message = 'Билет № {} счастливый'.format(ticket_number)
+        else:
+            message = 'Билет № {} не счастливый'.format(ticket_number)
+    else:
+        message = 'Номер билета неверный'
+    return message
+
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
---
+++ hw03_normal.py
@@ -0,0 +1,94 @@
+
+__author__ = 'Белинский Андрей Петрович'
+
+# Задание-1:
+# Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
+# Первыми элементами ряда считать цифры 1 1
+
+def fibonacci(n, m):
+    if n < 1:
+        return 'n должно быть больше 1'
+    else:
+        fib = [0, 1]
+        for i in range(2, m + 1):
+            fib.append(fib[i - 1] + fib[i - 2])
+        fib_n_m = fib[n:m+1]
+        return fib_n_m
+
+print(fibonacci(1,10))
+print()
+
+# Задача-2:
+# Напишите функцию, сортирующую принимаемый список по возрастанию.
+# Для сортировки используйте любой алгоритм (например пузырьковый).
+# Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
+
+def sort_to_max(origin_list):
+    stop_list = len(origin_list)
+    while stop_list > 0:
+        i = 1
+        while i < len(origin_list):
+            if origin_list[i] < origin_list[i - 1]:
+                origin_list[i], origin_list[i - 1] = origin_list[i - 1], origin_list[i]
+                stop_list += 1
+            else:
+                stop_list -= 1
+            i += 1
+    return origin_list
+
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
+print()
+
+# Задача-3:
+# Напишите собственную реализацию стандартной функции filter.
+# Разумеется, внутри нельзя использовать саму функцию filter.
+
+def my_filter(func, item):
+
+    new_item = [elem for elem in item if func(elem)]
+    if type(item) is tuple:
+        new_item = tuple(new_item)
+    if type(item) is set:
+        new_item = set(new_item)
+    if type(item) is str:
+        new_item = ''.join(new_item)
+    return new_item
+
+print(my_filter(lambda x: x <= 0, [2, 10, -12, 2.5, 20, -11, 4, 4, 0] ))
+print()
+
+# Задача-4:
+# Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
+# Определить, будут ли они вершинами параллелограмма.
+
+import math
+
+def pgram(a, b, c, d):
+
+    def paral(a, b, c, d):
+        ab = math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
+        cb = math.sqrt((b[0] - c[0])**2 + (b[1] - c[1])**2)
+        cd = math.sqrt((d[0] - c[0])**2 + (d[1] - c[1])**2)
+        ad = math.sqrt((d[0] - a[0])**2 + (d[1] - a[1])**2)
+
+        if ab == cd and cb == ad:
+            return 1
+        else:
+            return 0
+
+    def diag(a, b, c, d):
+
+        h_o1 = ((a[0] + c[0])/2, (a[1] + c[1])/2)
+        h_o2 = ((b[0] + d[0])/2, (b[1] + d[1])/2)
+
+        if h_o1 == h_o2:
+            return 1
+        else:
+            return 0
+
+    if paral(a, b, c, d) and diag(a, b, c, d):
+        print('Вершины A1{}, A2{}, A3{}, A4{} являются параллелограммом'.format(a, b, c, d))
+    else:
+        print('Точки не являются вершинами параллелограмма')
+
+pgram([-4, -2], [-3, 2], [7, -1], [6, -5])
